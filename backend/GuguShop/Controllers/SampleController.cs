cb0c8b43176849f86f66dd5b36576678eb91aad3
lengockyquang <kyquang97@gmail.com>
2022-05-25 22:35:04 +0700

    add test exception action

--- backend/GuguShop/Controllers/SampleController.cs
+++ backend/GuguShop/Controllers/SampleController.cs
@@ -25,4 +25,44 @@ public class SampleController: Controller
         var cipher = _cryptoService.Decrypt(encrypt, key);
         return Ok(cipher);
     }
+
+    [HttpGet("test-exception")]
+    public async Task<IActionResult> HandleTestException()
+    {
+        var allTasks = Task.WhenAll(new[]
+        {
+            GetExceptionAsync("Task 1"),
+            NormalTask("Task 2"),
+            GetExceptionAsync("Task 3"),
+        }); 
+        try
+        {
+            await allTasks;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: "+ex.Message);
+            if (allTasks.Exception != null)
+            {
+                foreach (var childTaskException in allTasks.Exception.InnerExceptions)
+                {
+                    Console.WriteLine("Child exception: "+childTaskException.Message);
+                }    
+            }
+        }
+        return Ok();
+    }
+
+    private static async Task NormalTask(string taskName)
+    {
+        await Task.Delay(5000);
+        Console.WriteLine(taskName+ " works!");
+    }
+
+    private static async Task GetExceptionAsync(string taskName)
+    {
+        await Task.Delay(5000);
+        throw new Exception("Exception is from " + taskName);
+    }
+    
 }
