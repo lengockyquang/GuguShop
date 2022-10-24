64e963331fd544aa592794663735309652104687
lengockyquang <kyquang97@gmail.com>
2022-10-24 23:27:17 +0700

    update migration connection string

--- backend/GuguShop.Migrator/MigratorDbContext.cs
+++ backend/GuguShop.Migrator/MigratorDbContext.cs
@@ -1,14 +1,23 @@
-﻿using GuguShop.Infrastructure.Extensions;
+﻿using GuguShop.Domain.Ums;
+using GuguShop.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace GuguShop.Migrator
 {
-    public class MigratorDbContext: DbContext
+    public class MigratorDbContext : IdentityDbContext<
+        User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
     {
         public MigratorDbContext(DbContextOptions options): base(options)
         {
         }
-
+        public override DbSet<User> Users { get; set; }
+        public override DbSet<Role> Roles { get; set; }
+        public override DbSet<UserClaim> UserClaims { get; set; }
+        public override DbSet<RoleClaim> RoleClaims { get; set; }
+        public override DbSet<UserToken> UserTokens { get; set; }
+        public override DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
